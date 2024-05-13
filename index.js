0ca6e83b49b6934c1520033b0f0185789f181a61
Shreya Porwal <111834212+porwalshreyaa@users.noreply.github.com>
2024-05-13 03:26:30 +0000

    update 1

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//   }
+  
+// function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+//   }
+  
+//   $(function() {
+//     $('.smoothScroll').click(function() {
+//       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//         var target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//         if (target.length) {
+//           $('html,body').animate({
+//             scrollTop: target.offset().top
+//           }, 1300); 
+//           return false;
+//         }
+//       }
+//     });
+//   });
+  
+  
